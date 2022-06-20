1ca10cf08b46ef5b58b53a65a3e99fd3de6aaffc
Ishan07Mostafa <ishan.mostafa7@gmail.com>
2022-06-20 14:08:50 +0600

    first commit

---
+++ Insertion Sort.py
@@ -0,0 +1,20 @@
+# For insertion sort
+# the space complexity is O(1)
+# while the time complexity is O(n^2)
+# it is a rather simple algorithm
+
+def insertionSort(array, n):
+    for i in range(1, n):
+        key = array[i]
+        j = i-1
+        while j >= 0 and key < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+
+
+a = [10, 1, 3, 7, 5]
+n = len(a)
+print(a)
+insertionSort(a, n)
+print(a)
